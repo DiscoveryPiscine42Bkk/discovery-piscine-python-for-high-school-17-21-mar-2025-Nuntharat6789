15942d3c569cc6f309a08ab24797e0f03c18dc1c
Nuntharat6789 <nuntharat6452@gmail.com>
2025-03-18 09:33:40 +0700

    Add files via upload

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,10 @@
+first_number = int(input("Enter the first number:\n"))
+second_number = int(input("Enter the second number:\n"))
+multiplication = first_number * second_number
+print(first_number, "x", second_number, "=", multiplication)
+if multiplication > 0:
+    print("The result is positive.")
+elif multiplication < 0:
+    print("The result is negative.")
+else:
+    print("The result is positive and negative.")
