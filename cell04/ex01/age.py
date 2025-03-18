9e03f1fc83f2ca164c4c6ad33d23a25feb005931
Nuntharat6789 <nuntharat6452@gmail.com>
2025-03-18 12:28:39 +0700

    Add files via upload

---
+++ cell04/ex01/age.py
@@ -0,0 +1,6 @@
+age = input("Please tell me your age: ")
+age_int = int(age)
+print(f"""You are currently {age_int} years old.
+In 10 years, you'll be {age_int + 10} years old.
+In 20 years, you'll be {age_int + 20} years old.
+In 30 years, you'll be {age_int + 30} years old.""")
