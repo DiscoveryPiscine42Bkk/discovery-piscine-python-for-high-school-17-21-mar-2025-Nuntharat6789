5c12c5af5d230a871205e10497ef267ce23425fc
Nuntharat6789 <nuntharat6452@gmail.com>
2025-03-18 21:41:42 +0700

    Update mult.py

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 first_number = int(input("Enter the first number:\n"))
 second_number = int(input("Enter the second number:\n"))
 multiplication = first_number * second_number
